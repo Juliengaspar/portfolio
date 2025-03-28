63e4c554ffd3ec2b5f72bec40ca497af96930c0b
Juliengaspar <julien.gaspar@student.hepl.be>
2025-03-28 16:53:33 +0100

    adeed-package-file & continu-the-header-fils

--- wp-content/themes/portfolio/functions.php
+++ wp-content/themes/portfolio/functions.php
@@ -1,8 +1,7 @@
 <?php
 /*ici va se trouver tous les function php qu'on développera pour plus tard */
-/*charge le champs srf*/
-include_once ('fields.php');
-
+// Charger les champs ACF exportés :
+include_once('./fields.php');
 
 //activer la sesion php
 if (session_status()===PHP_SESSION_NONE){
@@ -26,17 +25,20 @@ add_action( 'wp_enqueue_scripts', function() {
     // Remove inline global CSS on the front end.
     wp_dequeue_style( 'global-styles' );
 }, 20 );
-
-
-register_post_type('portfolio', [
-    'label' => 'Portfolio',
-    'description' => 'Mon portfolio pour mon projet pour le cours de design web ',
-    'menu_position' => 5,
+//
+//add_theme_support('post-thumbnails', ['portfolio']);
+//register_post_type( string $post_type, array|string $args = array() ): WP_Post_Type|WP_Error
+register_post_type('portfolio',[
+    'label' => 'Portfolios',
+    'description' => 'Mon portfolio pour mon projet pour le cours de design web',
+    'menu_position' => 1,
     'menu_icon' => 'dashicons-portfolio',
     'public' => true,
     'has_archive' => true,
     'rewrite' => [
-        'slug' => 'portfolio',
+        'slug' => 'portfolios',
     ],
     'supports' => ['title','excerpt','editor','thumbnail'],
 ]);
+
+?>
--- wp-content/themes/portfolio/header.php
+++ wp-content/themes/portfolio/header.php
@@ -6,15 +6,21 @@
 <html lang="fr-be">
 <head>
 <meta charset="utf-8">
-    <title>Titre de la page</title>
+    <title><?= wp_title('·', false, 'right') . get_bloginfo('name') ?></title><?php wp_head(); ?>
+    <link rel="icon" type="image/png" href="src/img/icone-portfolio-image.png">
 </head>
 <body>
 <nav class="nav__header">
     <h1 class="title__page">Titre invisible</h1>
     <ul>
-        <li></li>
+        <li>Aceuille</li>
+        <li>Projet</li>
+        <li>A Propos</li>
+    </ul>
+    <ul>
+        <li>contact</li>
     </ul>
 </nav>
 <header class="header">
-    <h2 class="header__title"></h2>
+    <h2 class="header__title">Julien Gaspar</h2>
 </header>
