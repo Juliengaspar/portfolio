6018b98750f99fa69a19c0017e241d478989434a
Juliengaspar <julien.gaspar@student.hepl.be>
2025-03-28 17:03:54 +0100

    added-file-footer.php

---
+++ wp-content/themes/portfolio/footer.php
@@ -0,0 +1,23 @@
+<footer>
+    <section class="footer__info">
+        <h2 class="email"></h2>
+        <nav class="navigation__footer">
+            <h2 class="navigation__title"></h2>
+            <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            </ul>
+        </nav>
+        <section class="contact">
+            <h2 class="contact title"></h2>
+            <button>Contactez mois</button>
+        </section>
+    </section>
+    <section class="copyright">
+        <h2 class="copyright__title"></h2>
+      <img class="reseauxSociaux" alt="icone represnetant un aplication mobile de resaux sociaux facebook ">
+      <img class="reseauxSociaux" alt="icone represnetant un aplication mobile de resaux sociaux twiter">
+      <img class="reseauxSociaux" alt="icone represnetant un aplication mobile de resaux sociaux linked ">
+    </section>
+</footer>
