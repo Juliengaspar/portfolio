1f5386b6802fda8c4f250456063cb6f163fc323f
Juliengaspar <julien.gaspar@student.hepl.be>
2025-03-28 14:14:26 +0100

    added-new-directorie & begin the header page in php

--- wp-content/themes/portfolio/header.php
+++ wp-content/themes/portfolio/header.php
@@ -1,2 +1,22 @@
 <?php
+
 ?>
+
+<!DOCTYPE html>
+<html lang="fr-be">
+<head>
+<meta charset="utf-8">
+    <title>Titre de la page</title>
+</head>
+<body>
+<nav>
+    <h1>Titre invisible</h1>
+    <ul>
+        <li></li>
+    </ul>
+</nav>
+<header>
+    <h2></h2>
+</header>
+</body>
+</html>
