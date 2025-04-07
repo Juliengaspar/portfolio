c42ab949c5af82b55b38624a5245415bbee4db0e
Juliengaspar <julien.gaspar@student.hepl.be>
2025-04-07 17:25:45 +0200

    added-fille-footer-html&css

--- wp-content/themes/portfolio/footer.php
+++ wp-content/themes/portfolio/footer.php
@@ -1,12 +1,32 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr-be">
+<head>
+    <meta charset="utf-8">
+    <!--    <title>--><?php //= wp_title('·', false, 'right') . get_bloginfo('name') ?><!--</title>--><?php //wp_head(); ?>
+    <title>Portfolion</title>
+    <link rel="icon" type="image/png" href="src/img/icone-portfolio-image.png">
+    <meta name="keywords" content="portfolio, Projet-web, Julien, competence, formation hepl, "/>
+    <meta name="author" content="Julien Gaspar"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="theme-color" content="#fafafa">
+    <link rel="stylesheet" href="./src/css/footer.css">
+    <!--    <script src="./src/js/main.js" type="module" defer></script>-->
+</head>
+<body>
 <footer>
     <section class="footer__info">
         <h2 class="email"></h2>
         <nav class="navigation__footer">
-            <h2 class="navigation__title"></h2>
+            <h2 class="navigation__title">Navigation</h2>
             <ul>
-            <li></li>
-            <li></li>
-            <li></li>
+            <li>Accueil</li>
+            <li>Projets</li>
+            <li>À propos</li>
+            <li>Contact</li>
             </ul>
         </nav>
         <section class="contact">
@@ -16,8 +36,8 @@
     </section>
     <section class="copyright">
         <h2 class="copyright__title"></h2>
-      <img class="reseauxSociaux" alt="icone represnetant un aplication mobile de resaux sociaux facebook ">
-      <img class="reseauxSociaux" alt="icone represnetant un aplication mobile de resaux sociaux twiter">
-      <img class="reseauxSociaux" alt="icone represnetant un aplication mobile de resaux sociaux linked ">
+      <img src="#" class="reseauxSociaux" alt="icone represnetant un aplication mobile de resaux sociaux facebook ">
+      <img src="#" class="reseauxSociaux" alt="icone represnetant un aplication mobile de resaux sociaux twiter">
+      <img src="#" class="reseauxSociaux" alt="icone represnetant un aplication mobile de resaux sociaux linked ">
     </section>
 </footer>
--- wp-content/themes/portfolio/header.php
+++ wp-content/themes/portfolio/header.php
@@ -6,19 +6,26 @@
 <html lang="fr-be">
 <head>
 <meta charset="utf-8">
-    <title><?= wp_title('·', false, 'right') . get_bloginfo('name') ?></title><?php wp_head(); ?>
+<!--    <title>--><?php //= wp_title('·', false, 'right') . get_bloginfo('name') ?><!--</title>--><?php //wp_head(); ?>
+    <title>Portfolion</title>
     <link rel="icon" type="image/png" href="src/img/icone-portfolio-image.png">
+    <meta name="keywords" content="portfolio, Projet-web, Julien, competence, formation hepl, "/>
+    <meta name="author" content="Julien Gaspar"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="theme-color" content="#fafafa">
+    <link rel="stylesheet" href="./src/css/header.css">
+<!--    <script src="./src/js/main.js" type="module" defer></script>-->
 </head>
 <body>
 <nav class="nav__header">
     <h1 class="title__page">Titre invisible</h1>
-    <ul>
+    <ul class="pricipale__list">
         <li>Aceuille</li>
         <li>Projet</li>
         <li>A Propos</li>
     </ul>
-    <ul>
-        <li>contact</li>
+    <ul class="second__lien">
+        <li class="contact__liens">contact</li>
     </ul>
 </nav>
 <header class="header">
