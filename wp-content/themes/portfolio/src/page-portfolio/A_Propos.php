97b179bfd9cc7e7f24d42e6677c3974e63990c79
Juliengaspar <julien.gaspar@student.hepl.be>
2025-05-14 16:14:04 +0200

    added-img&css-files

--- wp-content/themes/portfolio/src/page-portfolio/A_Propos.php
+++ wp-content/themes/portfolio/src/page-portfolio/A_Propos.php
@@ -1,42 +1,66 @@
 <?php
 get_header();
 ?>
+<?php
+get_header();
+?>
 <!doctype html>
 <html lang="fr-be">
+<link rel="stylesheet" href="../css/a_propos.css">
+<link rel="stylesheet" href="../css/reset.css">
 <head>
     <title></title>
 </head>
 <body>
-    <section>
-        <h2>A propos de moi</h2>
-            <p></p>
+<section class="about__me">
+    <div class="about__me__content">
+        <h2 class="title">A propos de moi</h2>
+        <p>
+            Je m'appelle Julien Gaspar, &eacute;tudiant en deuxième ann&eacute;e de Techniques Infographiques, option Web à l’HEPL Seraing (Belgique).
+            Passionn&eacute; de design et d&eacute;veloppement web, j’ai d&eacute;velopp&eacute; au fil de mes &eacute;tudes une maîtrise des dernières technologies et tendances, ainsi que des comp&eacute;tences solides pour concevoir des projets interactifs, esth&eacute;tiques et fonctionnels.
+            Ma passion pour le tennis de table se reflète dans le design de mes page.
+            À travers ce portfolio, je vous invite à d&eacute;couvrir mes r&eacute;alisations et mon &eacute;volution dans le domaine du web.
+        </p>
+    </div>
+    <div class="about__me__image">
+        <img src="./src/img/balle-ping-pong-transparant.png" alt="Photo de profil">
+    </div>
+</section>
+<section>
+    <h2 class="title">Mes comp&eacute;tences</h2>
+    <ul>
+        <li></li>
+    </ul>
+</section>
+<section class="school">
+    <h2 class="title school__path" >Mon parcours scolaire</h2>
+    <section class="school__info">
+        <div class="school__left">
+            <h3>Bachelier en d&eacute;veloppement web</h3>
+            <p>2022 -Aujourd’hui</p>
+            <p>Haute École de la Province de liège</p>
+        </div>
+        <div class="school__arrow"></div>
+        <div class="school__description">
+            <p class="contenus__description">En 2022, j’ai choisi de me réorienter vers l’infographie, un domaine qui allie le numérique et l’art, deux univers qui me passionnent.
+                Je suis actuellement étudiant en développement web à la HEPL. Ce bachelier me permet d’acquérir des compétences solides en programmation, design et gestion de projets web, me préparant ainsi à une carrière créative dans le secteur du digital.</p>
+        </div>
     </section>
-    <section>
-        <h2>Mes compétences</h2>
-            <ul>
-                <li></li>
-            </ul>
-    </section>
-    <section>
-    <h2>Mon parcours scolaire</h2>
-        <section>
-            <h3></h3>
-                <p></p>
-                <p></p>
-            <div>
-                <p></p>
-            </div>
-        </section>
-    </section>
+</section>
 
-    <section>
-        <h2>Mes passions</h2>
-        <p></p>
-        <img src="" alt="">
-    </section>
+<section class="passion__me">
+    <h2 class="title">Mes passions</h2>
+    <p>J’ai divers centres d’int&eacute;rêt ! Pendant mon temps libre, j’aime me d&eacute;tendre en cuisinant ou en peignant, une passion qui m’a d’ailleurs pouss&eacute; à &eacute;tudier l’histoire de l’art. Je pratique &eacute;galement le tennis de table et participe à des rencontres interclubs le week-end.</p>
+    <img src="./src/img/tennis-table_icone.png" alt="" class="passion__me__img">
+</section>
 
 </body>
 </html>
+<?php
+get_footer();
+?>
+
+
 <?php
 get_footer();
 ?>
--- wp-content/themes/portfolio/src/page-portfolio/Contact.php
+++ wp-content/themes/portfolio/src/page-portfolio/Contact.php
@@ -4,45 +4,65 @@ get_header();
 <!doctype html>
 <html lang="fr-be">
 <head>
-    <title></title>
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/contact.css">
+    <title>Formulaire__contacte</title>
 </head>
-<body>
-    <h2>Une question ?</h2>
-        <p>Je me ferai un plaisir de vous répondre.</p>
-    <form>
-        <label for="lastName">Nom<span>*</span></label>
-        <input type="text" name="lastName" id="lastName">
+<body class="contact__me">
+<h2 class="titile">Une question ?</h2>
+<p>Je me ferai un plaisir de vous r&eacute;pondre.</p>
+<section class="contact">
+    <form class="form">
+        <p class="form__elements">
+            <label class="lastName" for="lastName">Nom<span>*</span></label>
+            <input class="lastName" type="text" name="lastName" id="lastName">
+        </p>
 
-        <label for="firstName">Prenom<span>*</span></label>
-        <input type="text" name="firstName" id="firstName">
+        <p class="form__elements">
+            <label class="firstName" for="firstName">Prenom<span>*</span></label>
+            <input class="firstName" type="text" name="firstName" id="firstName">
+        </p>
 
-        <label for="email">Email<span>*</span></label>
-        <input type="text" name="email" id="email">
+        <p class="form__elements">
+            <label class="email" for="email">Email<span>*</span></label>
+            <input class="email" type="text" name="email" id="email">
+        </p>
 
-        <label for="subjetMessage">Sujet de votre message</label>
-        <select id="subjetMessage" name="subjetMessage">
-            <option>Proposition de projet ou collaboration</option>
-            <option>Demande d’informations supplémentaires</option>
-            <option>Opportunité d’embauche(CDI, CDD, stage…)</option>
-            <option>Autre(précisez dans le message)</option>
-        </select>
-        <label for="message">Message</label>
-        <input type="text" id="message" name="message">
-        <p><span>*</span>Eements obligatoires</p>
+        <p class="form__elements">
+            <label class="subjectMessage" for="subjetMessage">Sujet de votre message</label>
+            <select class="subjectMessage" id="subjetMessage" name="subjetMessage">
+                <option>Proposition de projet ou collaboration</option>
+                <option>Demande d’informations suppl&eacute;mentaires</option>
+                <option>Opportunit&eacute; d’embauche(CDI, CDD, stage…)</option>
+                <option>Autre(pr&eacute;cisez dans le message)</option>
+            </select>
+        </p>
 
-        <button type="submit">Envoyerr</button>
+        <p class="form__elements">
+            <label  class="message" for="message">Message</label>
+            <input  class="message" type="text" id="message" name="message">
+        </p>
+        <p class="form__elements"><span>*</span>Eements obligatoires</p>
+
+        <button class="submit" id="submit" type="submit">Envoyer</button>
     </form>
 
-    <h2>Coordonnées</h2>
-    <ul>
-        <li>jgasoar@oginformatique.be</li>
-        <li>+32 486 95 77 22</li>
-        <li>Rue d'evremont 318 4801 Stembert</li>
-    </ul>
-    <h2>Me suivre sur</h2>
-    <ul>
-        <li><img src=""alt=""></li>
-    </ul>
+    <section class="coordonnees">
+        <h2 class="coordonnee">Coordonn&eacute;es</h2>
+        <ul>
+            <li class="coordonnee__info">jgasoar&commat;oginformatique.be</li>
+            <li class="coordonnee__info">&#43;32 486 95 77 22</li>
+            <li class="coordonnee__info">Rue d'evremont 318 4801 Stembert</li>
+        </ul>
+        <h2 class="folow__me">Me suivre sur</h2>
+        <ul class="apps">
+            <li class="app"><img src="./src/img/facebook-icone.png" alt="app icone de facebook"></li>
+            <li class="app"><img src="./src/img/twitter-icone.png" alt="app icone de twitter"></li>
+            <li class="app"><img src="./src/img/ln-icone.png" alt="app icone de linked"></li>
+        </ul>
+    </section>
+</section>
+
 </body>
 </html>
 <?php
